ba3a79121a2a93ab8dfda77b4b527adfa84ec0de
Marlena Compton <marlena.compton@gmail.com>
2013-09-22 22:06:22 -0700

    set up all the things

---
+++ spec/LinkedListSpec.js
@@ -0,0 +1,21 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: marlena
+ * Date: 9/22/13
+ * Time: 9:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+describe("Player", function() {
+    var node;
+
+
+    beforeEach(function() {
+
+    });
+
+    it("should be true", function(){
+        blah = true;
+        expect(blah).toEqual(true);
+    });
+
+});
---
+++ src/LinkedList.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: marlena
+ * Date: 9/22/13
+ * Time: 9:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
