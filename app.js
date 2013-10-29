f3250ee0ac217f425c1120029159e4d4af124450
Marlena Compton <marlena.compton@gmail.com>
2013-10-28 18:48:43 -0700

    created express app

---
+++ app.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: marlena
+ * Date: 10/28/13
+ * Time: 6:45 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var express = require('express');
+var app = express();
